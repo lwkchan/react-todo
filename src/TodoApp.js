f426ef282f617491ddd8b045c0c4006007ca622e
Laura Chan <laurachan205@gmail.com>
2018-04-29 16:07:48 +0100

    Add constructor and parent methods to todoapp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -3,13 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/todoForm'
 import TodoList from './components/todoList'
+import Title from './components/title'
 
+window.id = 0;
 class TodoApp extends Component {
+  constructor(props){
+    // Pass props to parent class
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  addTodo(val){
+    const todo = {text: val, id: window.id++};
+    this.state.data.push(todo);
+    this.setState({data: this.state.data});
+  }
+
+  handleRemove(id){
+    const remainder = this.state.data.filter((todo) => {
+      if(todo.id !== id) return todo;
+    });
+
+    this.setState({data: remainder})
+  }
+
   render() {
     return (
       <div>
-        <TodoForm />
-        <TodoList todos={[{id:999, text: 'text'}]}/>
+        <Title />
+        <TodoForm addTodo={this.addTodo.bind(this)} />
+        <TodoList
+           todos = {this.state.data}
+           remove = {this.handleRemove.bind(this)}
+         />
       </div>
     );
   }
